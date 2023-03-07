5b23b3e6a1a77d0ef69b962f6b4359d7c4cb2528
mazenfteha <mazn34903@gmail.com>
2023-03-06 17:04:11 -0800

    setup all routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,25 @@ const getAllTasks = (req,res)=>{
     res.send('all items form controllers')
 }
 
+//create task
+const createTask = (req,res)=>{
+    res.json(req.body)
+}
+
+//get single task
+const getTask = (req, res) => {
+    res.json({id:req.params.id})
+}
+
+//update task
+const updateTask = (req,res)=>{
+    res.send('update task')
+}
+
+//delete task
+const deleteTask = (req,res)=>{
+    res.send('delete task')
+}
 module.exports={
-    getAllTasks
+    getAllTasks,createTask , getTask ,updateTask , deleteTask ,
 }
