c86d7bbcc797b10dc764201230549934a8103197
mazenfteha <mazn34903@gmail.com>
2023-03-06 18:04:39 -0800

    connect to db

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express =require('express');
 const app =express();
 const tasks =require('./routes/tasks')
+const connecDB = require('./db/connect')
+require('dotenv').config()
+
+
+//middleware
+app.use(express.json())
 
 //router
 app.get('/hello', (req,res)=>{
@@ -10,4 +16,16 @@ app.get('/hello', (req,res)=>{
 app.use('/api/v1/tasks',tasks)
 
 const port = 8081 ;
-app.listen(port, console.log(`Server is listen on port ${port}`));
+
+
+const start =async ()=>{
+    try {
+        await connecDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listen on port ${port}`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+start()
